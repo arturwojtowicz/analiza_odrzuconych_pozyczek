f1ff544c84b9d84390c40c6c27df6e5d968cbd51
dziabaku <artur.a.wojtowicz@gmail.com>
2019-02-03 19:41:36 +0100

    Dataset loaded correctly

--- projekt.R
+++ projekt.R
@@ -1,6 +1,17 @@
+# Zaciąganie bibliotek
 library("forecast")
 library("timeSeries")
 library("ggplot2")
 
-data = read.csv("/home/dziabaku/Studia/pakiety_statystyczne/Projekt/rejected_stats.csv", sep=",")
-head(data)
+# Ustawianie lokalnej pozycji do m.in zapisu plików oraz ich wczytywania
+setwd("/home/dziabaku/Studia/pakiety_statystyczne/Projekt/")
+
+# Wczytywanie danych
+data = read.csv("rejected_stats.csv", sep=",")
+
+# Utworzenie timeSeries
+data.ts = ts(data$Amount.requested, start = c(2007, 146), frequency=365.25) # Ponieważ 26 maja jest 146 dniem roku 2007
+
+# Utworzenie okna uczącego się oraz testowego
+data.train = window(data.ts, end = c(2011, 365))
+data.test = window(data.ts, start = c(2011, 366)) # Ponieważ w 2012 roku jest 366 dni
--- withdraw_important.py
+++ withdraw_important.py
@@ -1,25 +1,36 @@
+from datetime import timedelta, date
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def date_split(date):
+    return tuple([int(x) for x in date])
+
+collector = {2007: {}, 2008: {}, 2009: {}, 2010: {}, 2011: {}, 2012:{}}
+start_date = date(2007, 5, 26)
+end_date = date(2013, 1, 1)
+for single_date in daterange(start_date, end_date):
+    datatime = single_date.strftime("%Y-%m-%d")
+    datatime = datatime.replace('"','').split('-')
+    year, month, day = date_split(datatime)
+    if month not in collector[year].keys():
+        collector[year][month] = {}
+    collector[year][month][day] = 0
+
+
 dataset = open('RejectStatsA.csv', 'r')
 data = dataset.read().splitlines()
 output = 'Amount requested,Day,Month,Year\n'
-collector = {2007: {}, 2008: {}, 2009: {}, 2010: {}, 2011: {}, 2012:{}}
 dataset.close()
 
 for line in data[1::]:
     line = line.replace('"', '').split(',')
     datatime = line[1].split('-')
-    day = int(datatime[2])
-    month = int(datatime[1])
-    year = int(datatime[0])
+    year, month, day = date_split(datatime)
     value = float(line[0])
-
-    if month in collector[year].keys():
-        if day in collector[year][month].keys():
-            collector[year][month][day] += int(value)
-        else:
-            collector[year][month][day] = int(value)
-    else:
-        collector[year][month] = {}
-        collector[year][month][day] = int(value)
+    
+    collector[year][month][day] += int(value)
 
 for year, year_data in collector.items():
     for month, month_data in collector[year].items():
@@ -29,3 +40,4 @@ for year, year_data in collector.items():
 output_file = open('rejected_stats.csv', 'w')
 output_file.write(output)
 output_file.close()
+
