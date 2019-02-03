96bdc298a6eb5652159a9395c3b4c2f38b244063
dziabaku <artur.a.wojtowicz@gmail.com>
2019-02-03 19:06:23 +0100

    Initial commit

--- projekt.R
+++ projekt.R
@@ -2,13 +2,5 @@ library("forecast")
 library("timeSeries")
 library("ggplot2")
 
-data = read.csv("/home/dziabaku/Studia/pakiety_statystyczne/Projekt/RejectStatsA.csv", sep=",")
-data.ts = ts(data$Violent.crime, start = 1997, frequency = 1)
-data.train = window(data.ts, end = 2010)
-data.train = window(data.ts, start = 2011)
-png('main.png')
-plot.ts(data.train, xaxt='n',
-        main = "Szereg czasowy dla przestępstw w USA",
-        xlab="Rok", ylab="Liczba przestępstw")
-axis(1, at=1997:2010)
-dev.off()
+data = read.csv("/home/dziabaku/Studia/pakiety_statystyczne/Projekt/rejected_stats.csv", sep=",")
+head(data)
