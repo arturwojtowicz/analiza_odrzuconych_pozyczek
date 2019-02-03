12e0f73eca85bd122e7c682ec1fdfa057298efe3
dziabaku <artur.a.wojtowicz@gmail.com>
2019-02-03 20:07:46 +0100

    First plots

--- projekt.R
+++ projekt.R
@@ -15,3 +15,21 @@ data.ts = ts(data$Amount.requested, start = c(2007, 146), frequency=365.25) # Po
 # Utworzenie okna uczącego się oraz testowego
 data.train = window(data.ts, end = c(2011, 365))
 data.test = window(data.ts, start = c(2011, 366)) # Ponieważ w 2012 roku jest 366 dni
+
+# Utworzenie szeregu czasowego dla kwoty odrzucanych dziennie pożyczek przez Lending Club dla zbioru uczącego
+png('train_year_plot.png')
+plot.ts(data.train/1000000,
+        xaxt = 'n',
+        main = 'Szereg czasowy dla odrzucanych pożyczek przez Lending Club',
+        xlab = 'Rok',
+        ylab = 'Suma pożyczek (mln dolarów)')
+axis(1, at=2007:2012, las=2)
+dev.off()
+
+png('train_month_plot.png')
+monthplot(data.train, 
+          main = 'Wykres wahań sezonowych', 
+          xlab = 'Miesiąc',
+          ylab = 'Suma pożyczek (mln dolarów)')
+
+png('train_day_plot.png')
