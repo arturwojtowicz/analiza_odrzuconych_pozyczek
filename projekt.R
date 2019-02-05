cb38109d8c9610be6530a72df4afebf8b454849c
dziabaku <artur.a.wojtowicz@gmail.com>
2019-02-05 17:32:20 +0100

    Monthly data included into R, new plots generating

--- projekt.R
+++ projekt.R
@@ -1,23 +1,18 @@
 # Zaciąganie bibliotek
-library("readr") #Load our data
-library("dplyr") # Data Wrangling
 library("ggplot2") #Visualization
-library("ggfortify") #Visualization
 library("timeSeries") #Statistical Tests for Time Series data
 library("forecast") #Time Series Forecasting
-library(DT) # View our Raw Data
 
-install.packages('dplyr')
-install.packages('ggfortify')
-install.packages('DT')
 # Ustawianie lokalnej pozycji do m.in zapisu plików oraz ich wczytywania
 setwd("/home/dziabaku/Studia/pakiety_statystyczne/Projekt/")
 
 # Wczytywanie danych
-data = read.csv("rejected_stats.csv", sep=",")
+data = read.csv("rejected_stats.csv", sep=",") # Dane dzienne
+data_monthly = read.csv("rejected_monthly_stats.csv", sep=",") # Dane miesięczne
 
 # Utworzenie timeSeries
 data.ts = ts(data$Amount.requested, start = c(2007, 146), frequency=365.25) # Ponieważ 26 maja jest 146 dniem roku 2007
+data_monthly.ts = ts(data_monthly$Amount.requested, start = c(2007, 5), frequency = 12)
 
 # Utworzenie okna uczącego się oraz testowego
 data.train = window(data.ts, end = c(2011, 365))
@@ -36,7 +31,10 @@ dev.off()
 plots = stl(data.train,s.window="periodic")
 
 png('train_season_plot.png')
-ggseasonplot(data.train, year.labels=TRUE, year.labels.left=FALSE) +
-             ylab("Kwota odrzuconych pożyczek (mln dolarów)") +
-             ggtitle("Wykres wahań sezonowych")
+plot.ts(plots$time.series[,'seasonal']/1000000,
+        xaxt = 'n',
+        main = 'Wykres wahań sezonowych',
+        xlab = 'Rok',
+        ylab = 'Kwota odrzuconych pożyczek (mln dolarów)')
+axis(1, at=2007:2012, las=2)
 dev.off()
